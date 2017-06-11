ab087089cfd16e6a6640dff95e1f59f40fb58a9e
Ernesto Rojas <ernesto.rojas.21st@gmail.com>
2017-06-11 06:05:26 -0700

    Created Angry Boss Simulator and Table of Contents example

---
+++ ch6/angryBoss.rb
@@ -0,0 +1,3 @@
+puts 'Why are you in my office?!?!'
+response = gets.chomp
+puts 'WHAT DO YOU MEAN "' + response.upcase + '"? YOU ARE FIRED!'
---
+++ ch6/tableOfContents.rb
@@ -0,0 +1,5 @@
+length = 80
+puts ('Table of Contents'.center(length))
+puts ('Chapter 1: Getting Started'.ljust(length/2) + 'page 1'.rjust(length/2))
+puts ('Chapter 2: Numbers'.ljust(length/2) + 'page 2'.rjust(length/2))
+puts ('Chapter 3: Letters'.ljust(length/2) + 'page 3'.rjust(length/2))
