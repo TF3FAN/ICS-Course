13d252376ebb10b54ea9aa145dbe18ff53a94592
Ernesto Rojas <ernesto.rojas.21st@gmail.com>
2017-06-11 17:29:01 -0700

    Modified ch2 files to account for leap years and removed ch5 files from the ch2 folder

--- ch2/biggerBetterFavoriteNumber.rb
+++
@@ -1,4 +0,0 @@
-puts 'What is your favorite number?'
-number = gets.chomp
-betternumber = number.to_i + 1
-puts 'Really? ' + betternumber.to_s + ' is bigger and better!'
--- ch2/fullNameGreeting.rb
+++
@@ -1,7 +0,0 @@
-puts 'Hello there! What is your first name?'
-firstname = gets.chomp
-puts 'What a great name' + ' ' + firstname + ' ' + 'is! Do you have a middle name? If so, enter it below with a space afterwards. If not, just press enter.'
-middlename = gets.chomp
-puts 'All right! What is your last name?'
-lastname = gets.chomp
-puts 'It is nice to met you, ' + firstname + ' ' + middlename + lastname + '!'
